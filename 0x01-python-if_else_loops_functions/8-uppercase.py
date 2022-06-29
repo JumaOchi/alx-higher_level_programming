fd7743884a48582968ce6e7074113868d9031aeb
JumaOchi <ochiengjuma95@gmail.com>
2022-06-29 04:04:10 -0700

    convertion from lower to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in range(len(str)):
+        if str[i] >= 'a' and str[i] < '{':
+            char = chr(ord(str[i]) - 32)
+        else:
+            char = str[i]
+        print('{}'.format(char), end='')
+    print('')
