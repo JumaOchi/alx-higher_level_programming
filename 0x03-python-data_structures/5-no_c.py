2447c30e55ca6c60cd752fc8fa30ae322ede7e5f
JumaOchi <ochiengjuma95@gmail.com>
2022-07-04 16:09:31 -0700

    This is my commit message

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+
 def no_c(my_string):
-	for i in my_string:
-		if i = ord('c') or i = ord('C'):
-			del my_string[i]
- 	new_string = del my_string
-        return new_string
+
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
