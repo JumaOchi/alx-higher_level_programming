a222a10abd6719692051d94ca9ad3c90fdcf505f
JumaOchi <ochiengjuma95@gmail.com>
2022-07-04 14:54:58 -0700

    This is my commit message

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-	new_list = my_lists.insert(idx, element)
-	return new_list
+    
+    if idx < 0:
+        new_list = my_list.copy()
+    elif idx > len(my_list):
+        new_list = my_list.copy()
+    else:
+        new_list = my_list.insert(idx, element)
+    return new_list
