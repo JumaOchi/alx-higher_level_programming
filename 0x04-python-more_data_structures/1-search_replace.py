5a94f93a784701ae514bb07167e941ee2d86e757
JumaOchi <ochiengjuma95@gmail.com>
2022-07-06 16:24:46 -0700

    update serch and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,6 @@
 
 
 def search_replace(my_list, search, replace):
-	new_list = [replace if i == search else search for i in my_list]
-	return new_list
+
+    new_list = list(map(lambda x: x.replace('search', 'replace'), my_list)
+    return new_list
