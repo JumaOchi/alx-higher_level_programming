518e76ae3af04590a8b556eb8626703a40c01edc
JumaOchi <ochiengjuma95@gmail.com>
2022-07-06 15:14:23 -0700

    Update to data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,7 @@ def square_matrix_simple(matrix=[]):
     
     new_matrix = []
     for item in matrix:
-        new_item = list(map(lambda x: X**2, item))
+        new_item = list(map(lambda x: x ** 2, item))
         new_matrix.append(new_item)
 
     return new_matrix
