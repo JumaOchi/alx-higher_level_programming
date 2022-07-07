99878b4f02fbdfc7a4774835ec2795f4f3ee62c9
JumaOchi <ochiengjuma95@gmail.com>
2022-07-06 17:24:11 -0700

    Update

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def only_diff_elements(set_1, set_2):
-	new_set = {x for x in set_1 ^ set_2}
-	return new_set
+    return set(list({x for x in set_1 if x not in set_2}) +
+               list({x for x in set_2 if x not in set_1}))
