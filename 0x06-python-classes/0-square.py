dda7d1a69d8401ee503cc004e5a79a370fac99ad
JumaOchi <ochiengjuma95@gmail.com>
2022-07-19 15:28:46 -0700

    Update to empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""
+My first square
+"""
 
 
-class square:
-    """A square is a 4sided shape all with
-       equal length"""
+class Square:
+    """define blank class"""
     {}
