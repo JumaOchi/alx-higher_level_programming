7905bf96c8d2bcfbb4d8a8f0fd8aeacf027f0d33
JumaOchi <ochiengjuma95@gmail.com>
2022-07-26 14:44:07 -0700

    update to commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,16 +3,16 @@
 
 
 def add_integer(a, b=98):
-"""defining function variables"""
+    """defining function variables"""
 
-if (isinstance(a, int) or isinstance(a, float) and\
-        (isinstance(b, int) or isinstance(b, float)):
-        if isinstance(a, float):
-            a = int(a)
-        if isinstance(b, float):
-            b = int(b)
-        return a + b
-        elif not (isinstance(a, int) or isinstance(a, float)):
-            raise TypeError("a must be an integer")
-        elif not (isinstance(b, int) or isinstance(b, float)):
-            raise TypeError("b must be an integer")
+    if (isinstance(a, int) or isinstance(a, float) and\
+            (isinstance(b, int) or isinstance(b, float)):
+            if isinstance(a, float):
+                a = int(a)
+            if isinstance(b, float):
+                b = int(b)
+            return a + b
+    elif not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    elif not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
