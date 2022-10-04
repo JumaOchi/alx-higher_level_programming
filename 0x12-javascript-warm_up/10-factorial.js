6adef8b2bc336d9615c742ec799356c77c72e3f0
JumaOchi <ochiengjuma95@gmail.com>
2022-10-05 02:34:39 +0300

    Quick fix on factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 const myArgs = process.argv.slice(2);
 
 function factorial(n) {
@@ -7,8 +6,9 @@ function factorial(n) {
   if (n === 0) return 1;
   return n * factorial(n-1);
   }
+
 if (isNaN(myArgs[0])) {
   console.log("1");
   } else {
     console.log(factorial(parseInt(myArgs[0])));
-    }
+  }
