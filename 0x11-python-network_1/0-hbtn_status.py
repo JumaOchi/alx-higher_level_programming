6e125b5af506ba08ee1e0fd5cb7d1da6fcb18edf
JumaOchi <ochiengjuma95@gmail.com>
2022-10-26 20:13:19 +0300

    update initial commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
-        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
+        print("\t- type: {}$\n\t- content: {}$\n\t- utf8 content: {}$".
               format(html.__class__, html, html.decode('utf-8')))
