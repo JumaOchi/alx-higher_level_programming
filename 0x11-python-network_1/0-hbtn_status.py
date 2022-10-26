15e5650818a81d2ee27f264f9050bc2b5799dda9
JumaOchi <ochiengjuma95@gmail.com>
2022-10-26 22:01:42 +0300

    update correct url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
