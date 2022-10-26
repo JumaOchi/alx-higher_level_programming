c3066adb353bc135f6007d742c7af83fdbab7721
JumaOchi <ochiengjuma95@gmail.com>
2022-10-26 21:56:32 +0300

    update task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     import requests
 
     response = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}".format(
+    print("Body response:$\n\t- type: {}$\n\t- content: {}$".format(
         response.text.__class__, response.text))
