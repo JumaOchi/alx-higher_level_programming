e8d8e0fbabdebc894c1f6bb187a735a218fe0b29
JumaOchi <ochiengjuma95@gmail.com>
2022-11-29 04:21:03 +0300

    j-querry update solution

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,10 @@
+//
+$.ajax(
+  {
+    url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+    type: 'GET',
+    dataType: 'json',
+    success: function (response) { $('#character').text(response.name); }
+  }
+);
+
